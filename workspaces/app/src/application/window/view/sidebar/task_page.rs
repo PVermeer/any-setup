cf87a23be0eb647f9e23e2e08c8d404d8655fabb
Pim Vermeer <pim.vermeer@gmail.com>
2026-05-17 12:31:26 +0000

    feat(tasks-page): added some ui updates and refactors

--- workspaces/app/src/application/window/view/sidebar/task_page.rs
+++ workspaces/app/src/application/window/view/sidebar/task_page.rs
@@ -4,26 +4,26 @@ use crate::application::{
     pages::PrefNavPageBuild,
     task_manager::{TaskEvent, TaskStatus, action_runner::ActionResult},
 };
-use gtk::{
-    Align, IconSize, Image, Label, Orientation,
-    prelude::{BoxExt, WidgetExt},
-};
+use gtk::{Image, prelude::WidgetExt};
 use libadwaita::{
-    NavigationPage, PreferencesGroup, PreferencesRow, Spinner,
-    prelude::{PreferencesGroupExt, PreferencesPageExt},
+    ActionRow, NavigationPage, PreferencesGroup, Spinner,
+    prelude::{ActionRowExt, PreferencesGroupExt, PreferencesPageExt},
 };
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{
+    cell::RefCell,
+    ops::{Deref, DerefMut},
+    rc::Rc,
+};
 use tracing::error;
 
 #[derive(Debug)]
 struct TaskUi {
-    task_row: PreferencesRow,
+    task_row: ActionRow,
     run_id: String,
     success_icon: Image,
     fail_icon: Image,
     running_icon: Spinner,
     results: Option<Vec<ActionResult>>,
-    error: Option<String>,
 }
 impl TaskUi {
     fn from_event(task_event: &TaskEvent) -> Self {
@@ -38,92 +38,101 @@ impl TaskUi {
             fail_icon,
             running_icon,
             results: None,
-            error: None,
         }
     }
 
-    fn update(&self) {
-        if self.results.is_some() {
-            self.success_icon.set_visible(true);
-            self.fail_icon.set_visible(false);
-            self.running_icon.set_visible(false);
-        }
-        if self.error.is_some() {
-            self.success_icon.set_visible(false);
-            self.fail_icon.set_visible(true);
-            self.running_icon.set_visible(false);
-            self.task_row.add_css_class("error");
-        }
+    fn set_progress(&mut self, action: Option<&str>, action_nr: Option<i32>, total_actions: i32) {
+        let Some(action) = action else {
+            self.task_row.set_subtitle("");
+            return;
+        };
+
+        let progress = if let Some(task_nr) = action_nr {
+            format!("{task_nr} / {total_actions} : {action}")
+        } else {
+            String::new()
+        };
+
+        self.task_row.set_subtitle(&progress);
     }
 
-    fn build_task_row(title: &str) -> (PreferencesRow, Image, Image, Spinner) {
-        let margin = 12;
+    fn set_success(&mut self, results: &[ActionResult]) {
+        self.results = Some(results.to_owned());
 
-        let task_content_box = gtk::Box::builder()
-            .orientation(Orientation::Horizontal)
-            .margin_top(margin)
-            .margin_bottom(margin)
-            .margin_start(margin)
-            .margin_end(margin)
-            .spacing(margin)
-            .height_request(70)
-            .build();
+        self.task_row.set_subtitle("");
+        self.success_icon.set_visible(true);
+        self.fail_icon.set_visible(false);
+        self.running_icon.set_visible(false);
+    }
 
-        let task_icon = Image::builder()
-            .icon_name("system-run-symbolic")
-            .icon_size(IconSize::Large)
-            .build();
-        task_content_box.append(&task_icon);
+    fn set_error(&mut self, error: &anyhow::Error) {
+        self.task_row.set_subtitle(&error.to_string());
+        self.task_row.add_css_class("error");
 
-        let task_box = gtk::Box::builder()
-            .orientation(Orientation::Vertical)
-            .spacing(margin)
-            .build();
+        self.success_icon.set_visible(false);
+        self.fail_icon.set_visible(true);
+        self.running_icon.set_visible(false);
+    }
 
-        let title = Label::builder().label(title).build();
-        task_box.append(&title);
-
-        let task_status_box = gtk::Box::builder()
-            .orientation(Orientation::Horizontal)
-            .spacing(margin)
-            .halign(Align::End)
-            .hexpand(true)
-            .build();
+    fn build_task_row(title: &str) -> (ActionRow, Image, Image, Spinner) {
+        let task_row = ActionRow::builder().title(title).build();
 
         let success_icon = Image::builder()
             .icon_name("object-select-symbolic")
             .css_classes(["success"])
-            .icon_size(IconSize::Large)
             .visible(false)
             .build();
         let fail_icon = Image::builder()
             .icon_name("process-stop-symbolic")
             .css_classes(["error"])
-            .icon_size(IconSize::Large)
             .visible(false)
             .build();
-        let running_icon = Spinner::builder()
-            .height_request(30)
-            .width_request(30)
-            .build();
+        let running_icon = Spinner::new();
 
-        task_status_box.append(&success_icon);
-        task_status_box.append(&fail_icon);
-        task_status_box.append(&running_icon);
-
-        task_content_box.append(&task_box);
-        task_content_box.append(&task_status_box);
-
-        let task_row = PreferencesRow::builder().child(&task_content_box).build();
+        task_row.add_prefix(&Image::from_icon_name("system-run-symbolic"));
+        task_row.add_suffix(&running_icon);
+        task_row.add_suffix(&success_icon);
+        task_row.add_suffix(&fail_icon);
 
         (task_row, success_icon, fail_icon, running_icon)
+    }
+}
+
+struct TaskVec(Vec<TaskUi>);
+trait TaskVecExt {
+    fn find_task_mut(&mut self, id: &str) -> Option<&mut TaskUi>;
+}
+impl Deref for TaskVec {
+    type Target = Vec<TaskUi>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl DerefMut for TaskVec {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+impl TaskVecExt for TaskVec {
+    fn find_task_mut(&mut self, id: &str) -> Option<&mut TaskUi> {
+        let last_matches = matches!(
+            self.last(),
+            Some(task) if task.run_id == id
+        );
+
+        if last_matches {
+            self.last_mut()
+        } else {
+            self.iter_mut().find(|task| task.run_id == id)
+        }
     }
 }
 
 pub struct TaskPage {
     nav_page: NavigationPage,
     tasks_pref_group: PreferencesGroup,
-    tasks: RefCell<HashMap<String, TaskUi>>,
+    tasks: RefCell<TaskVec>,
 }
 impl NavPage for TaskPage {
     fn get_navpage(&self) -> &NavigationPage {
@@ -155,7 +164,7 @@ impl TaskPage {
         Rc::new(Self {
             nav_page,
             tasks_pref_group,
-            tasks: RefCell::new(HashMap::new()),
+            tasks: RefCell::new(TaskVec(Vec::new())),
         })
     }
 
@@ -178,44 +187,66 @@ impl TaskPage {
                     self_clone.set_task_error(&task_event.run_id, error);
                 }
 
-                _ => {}
+                TaskStatus::Progress {
+                    action,
+                    action_nr,
+                    total_actions,
+                    progress: _,
+                    status: _,
+                } => self_clone.set_task_progress(
+                    &task_event.run_id,
+                    action.as_deref(),
+                    *action_nr,
+                    *total_actions,
+                ),
             });
     }
 
     fn add_task(self: &Rc<Self>, task_event: &TaskEvent) {
         let task_ui = TaskUi::from_event(task_event);
-        self.tasks_pref_group.add(&task_ui.task_row);
+        let task_ui_run_id = task_ui.run_id.clone();
+        self.tasks.borrow_mut().push(task_ui);
 
-        let replaced_task = self
-            .tasks
-            .borrow_mut()
-            .insert(task_ui.run_id.clone(), task_ui);
-
-        if replaced_task.is_some() {
-            error!(
-                ?task_event,
-                "Task already added to the ui, this should not happen!"
-            );
+        // Reset task list so new tasks are added on top
+        for task in self.tasks.borrow().iter().rev() {
+            if task.run_id != task_ui_run_id {
+                self.tasks_pref_group.remove(&task.task_row);
+            }
+            self.tasks_pref_group.add(&task.task_row);
         }
+    }
+
+    fn set_task_progress(
+        self: &Rc<Self>,
+        id: &str,
+        action: Option<&str>,
+        action_nr: Option<i32>,
+        total_actions: i32,
+    ) {
+        let mut tasks_borrow_mut = self.tasks.borrow_mut();
+        let Some(task) = tasks_borrow_mut.find_task_mut(id) else {
+            error!("Failed to get ui task by id for progress");
+            return;
+        };
+
+        task.set_progress(action, action_nr, total_actions);
     }
 
     fn set_task_results(self: &Rc<Self>, id: &str, results: &[ActionResult]) {
         let mut tasks_borrow_mut = self.tasks.borrow_mut();
-        let Some(task) = tasks_borrow_mut.get_mut(id) else {
-            error!("Failed to get ui task by id to set error");
+        let Some(task) = tasks_borrow_mut.find_task_mut(id) else {
+            error!("Failed to get ui task by id for result");
             return;
         };
-        task.results = Some(results.to_owned());
-        task.update();
+        task.set_success(results);
     }
 
     fn set_task_error(self: &Rc<Self>, id: &str, error: &anyhow::Error) {
         let mut tasks_borrow_mut = self.tasks.borrow_mut();
-        let Some(task) = tasks_borrow_mut.get_mut(id) else {
-            error!("Failed to get ui task by id to set error");
+        let Some(task) = tasks_borrow_mut.find_task_mut(id) else {
+            error!("Failed to get ui task by id for error");
             return;
         };
-        task.error = Some(error.to_string());
-        task.update();
+        task.set_error(error);
     }
 }
