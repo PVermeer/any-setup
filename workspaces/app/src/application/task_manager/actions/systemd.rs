4164d678fdd96609d0d7df59855e1c80fb31b1bd
Pim Vermeer <pim.vermeer@gmail.com>
2026-09-13 14:32:23 +0000

    feat(systemd): added alias handling

--- workspaces/app/src/application/task_manager/actions/systemd.rs
+++ workspaces/app/src/application/task_manager/actions/systemd.rs
@@ -1,6 +1,6 @@
 use super::ActionState;
 use crate::application::task_manager::actions::IsAction;
-use anyhow::{Context, Result, anyhow};
+use anyhow::{Context, Result, anyhow, bail};
 use common::utils;
 use serde::{Deserialize, Serialize};
 use std::{fmt::Display, process::Command, str::FromStr};
@@ -108,17 +108,53 @@ impl FromStr for IsEnabledOutput {
     }
 }
 impl IsEnabledOutput {
-    fn to_action_state(&self, action: &SystemdAction) -> ActionState {
+    fn from_action(action: &SystemdAction) -> Result<Self> {
+        let output = action
+            .get_check_command()
+            .output()
+            .context("Failed to run command")?;
+        let stdout = utils::command::parse_output(&output.stdout);
+
+        Self::from_str(&stdout)
+    }
+
+    fn to_action_state(&self, action: &SystemdAction, recursive_depth: Option<u32>) -> ActionState {
+        fn handle_alias(action: &SystemdAction, recursive_depth: Option<u32>) -> ActionState {
+            let depth = recursive_depth.unwrap_or(0);
+            if depth > 10 {
+                error!(%action, depth, "Reached max recursive depth trying to resolve an alias");
+                return ActionState::UnAvailable;
+            }
+
+            let Ok(resolved_unit_name) = action.resolve_alias() else {
+                error!("Failed to resolve alias for");
+                return ActionState::UnAvailable;
+            };
+            let mut action_clone = action.clone();
+            match &mut action_clone {
+                SystemdAction::Enable { unit, .. } | SystemdAction::Disable { unit, .. } => {
+                    *unit = resolved_unit_name;
+                }
+            }
+            let Ok(new_is_enabled_output) = IsEnabledOutput::from_action(&action_clone) else {
+                return ActionState::UnAvailable;
+            };
+
+            new_is_enabled_output.to_action_state(&action_clone, Some(depth + 1))
+        }
+
         match action {
             SystemdAction::Enable { .. } => match self {
                 Self::Enabled => ActionState::Done,
                 Self::Disabled => ActionState::Available,
+                Self::Alias => handle_alias(action, recursive_depth),
                 _ => ActionState::UnAvailable,
             },
 
             SystemdAction::Disable { .. } => match self {
-                Self::Disabled => ActionState::Done,
                 Self::Enabled => ActionState::Available,
+                Self::Disabled => ActionState::Done,
+                Self::Alias => handle_alias(action, recursive_depth),
                 _ => ActionState::UnAvailable,
             },
         }
@@ -197,19 +233,12 @@ impl IsAction for SystemdAction {
     }
 
     fn get_status(&self) -> Result<ActionState> {
-        debug!(action = %self, "Running check command");
+        debug!(action = %self, "Running status command");
 
-        let output = self
-            .get_check_command()
-            .output()
-            .context("Failed to run command")?;
+        let is_enabled_output = IsEnabledOutput::from_action(self)?;
+        let action_state = is_enabled_output.to_action_state(self, None);
 
-        // Cannot test for success, it will be non-zero for disabled
-        let stdout = utils::command::parse_output(&output.stdout);
-        let is_enabled_output = IsEnabledOutput::from_str(&stdout)?;
-        let action_state = is_enabled_output.to_action_state(self);
-
-        debug!(action = %self, state = %stdout, action_state = %action_state, "Action state");
+        debug!(action = %self, is_enabled_output = %is_enabled_output, action_state = %action_state, "Action state");
 
         Ok(action_state)
     }
@@ -233,12 +262,40 @@ impl SystemdAction {
             }
         }
     }
+
+    fn resolve_alias(&self) -> Result<String> {
+        match self {
+            SystemdAction::Enable { unit, scope, .. }
+            | SystemdAction::Disable { unit, scope, .. } => {
+                let mut command = Command::new("systemctl");
+                command
+                    .arg(scope.to_arg())
+                    .arg("show")
+                    .arg("--property=Id")
+                    .arg("--value")
+                    .arg(unit);
+
+                let output = command
+                    .output()
+                    .context("Failed to run systemd alias resolve command")?;
+
+                if !output.status.success() {
+                    let message = format!("Failed to resolve systemd unit alias for: {unit}");
+                    error!(message);
+                    bail!(message);
+                }
+
+                Ok(utils::command::parse_output(&output.stdout))
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 #[allow(clippy::unwrap_used)]
 mod tests {
     use super::*;
+    use tracing_test::traced_test;
 
     fn enable_action() -> SystemdAction {
         SystemdAction::Enable {
@@ -376,17 +433,17 @@ mod tests {
         let action = enable_action();
 
         assert_eq!(
-            IsEnabledOutput::Enabled.to_action_state(&action),
+            IsEnabledOutput::Enabled.to_action_state(&action, None),
             ActionState::Done
         );
 
         assert_eq!(
-            IsEnabledOutput::Disabled.to_action_state(&action),
+            IsEnabledOutput::Disabled.to_action_state(&action, None),
             ActionState::Available
         );
 
         assert_eq!(
-            IsEnabledOutput::NotFound.to_action_state(&action),
+            IsEnabledOutput::NotFound.to_action_state(&action, None),
             ActionState::UnAvailable
         );
     }
@@ -396,18 +453,44 @@ mod tests {
         let action = disable_action();
 
         assert_eq!(
-            IsEnabledOutput::Disabled.to_action_state(&action),
+            IsEnabledOutput::Disabled.to_action_state(&action, None),
             ActionState::Done
         );
 
         assert_eq!(
-            IsEnabledOutput::Enabled.to_action_state(&action),
+            IsEnabledOutput::Enabled.to_action_state(&action, None),
             ActionState::Available
         );
 
         assert_eq!(
-            IsEnabledOutput::NotFound.to_action_state(&action),
+            IsEnabledOutput::NotFound.to_action_state(&action, None),
             ActionState::UnAvailable
         );
+    }
+
+    #[test]
+    #[traced_test]
+    fn enable_alias_reaches_max_recursion_depth() {
+        let action = enable_action();
+
+        assert_eq!(
+            IsEnabledOutput::Alias.to_action_state(&action, Some(11)),
+            ActionState::UnAvailable
+        );
+
+        assert!(logs_contain("Reached max recursive depth"));
+    }
+
+    #[test]
+    #[traced_test]
+    fn disable_alias_reaches_max_recursion_depth() {
+        let action = disable_action();
+
+        assert_eq!(
+            IsEnabledOutput::Alias.to_action_state(&action, Some(11)),
+            ActionState::UnAvailable
+        );
+
+        assert!(logs_contain("Reached max recursive depth"));
     }
 }
